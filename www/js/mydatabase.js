2ee5bcbcf4109d1607555b4671517124579609ee
Jyoti <abstract_jyoti@rediffmail.com>
2017-11-11 12:44:16 +0530

    platform adding

---
+++ www/js/mydatabase.js
@@ -0,0 +1,21 @@
+//VARIABLES NEEDED
+
+var adminurl = "my url";
+
+
+var db = openDatabase('hniretailer', '1.0', 'hnireyailer DB', 50 * 1024 * 1024);
+
+
+var mydatabase = angular.module('mydatabase', [])
+    .factory('MyDatabase', function ($http, $location, MyServices) {
+
+
+
+        return {
+           
+        }
+    });
+
+
+
+
---
+++ www/js/services.js
@@ -0,0 +1,16 @@
+
+var adminurl ="my url";
+var myservices = angular.module('myservices', [])
+
+
+.factory('MyServices', function ($http, $location) {
+    
+
+    
+
+    return {
+       
+
+
+    }
+});
